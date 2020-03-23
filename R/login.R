aa9202208dd31fc536ab091d2c7d0ff08d159e9d
w4356y <w4356y@163.com>
2020-03-23 19:58:48 +0800

    format file login.R;

--- R/login.R
+++ R/login.R
@@ -1,7 +1,9 @@
 create_login_confirm <- function(session, input, output){
   event <- observeEvent(input$confirmLogin, {
     if(input$login.user == "" | input$login.password == "" | input$anhao == ""){
-      showNotification("Warnings!","Please confirm username, password and passcode are specifued.", type ="message")
+      showNotification("Warnings!",
+                       "Please confirm username, password and passcode are specifued.", 
+                       type ="message")
     }
     req(input$login.user)
     #browser()
@@ -11,18 +13,34 @@ create_login_confirm <- function(session, input, output){
     if(input$login.user %in% c('messi','manning')){
       if(input$login.password == "test"){
         if(input$anhao == "Virus"){
-          shinyWidgets::sendSweetAlert(session, title = "Wow! Congrats!",text = "Login successfully.", type = "success")
+          shinyWidgets::sendSweetAlert(session, 
+                                       title = "Wow! Congrats!", 
+                                       text = "Login successfully.", 
+                                       type = "success"
+                                       )
           shinyjs::show(selector = "a[data-value = 'BioMeta']")
           shinyjs::show(selector = "a[data-value = 'BioAnalysis']")
           shinyjs::show(selector = "a[data-value = 'BioAI']")
         }else{
-          shinyWidgets::sendSweetAlert(session, title = "Sorry! Wrong!","Please check your passcode.", type = "error")
+          shinyWidgets::sendSweetAlert(session, 
+                                       title = "Sorry! Wrong!",
+                                       "Please check your passcode.",
+                                       type = "error"
+                                       )
         }
       }else{
-        shinyWidgets::sendSweetAlert(session, title = "Sorry! Wrong!","Please check your password.", type = "error")
+        shinyWidgets::sendSweetAlert(session, 
+                                     title = "Sorry! Wrong!", 
+                                     "Please check your password.", 
+                                     type = "error"
+                                     )
       }
     }else{
-      shinyWidgets::sendSweetAlert(session, title = "Sorry! Wrong!","Please check your username.", type = "error")
+      shinyWidgets::sendSweetAlert(session, 
+                                   title = "Sorry! Wrong!",
+                                   "Please check your username.", 
+                                   type = "error"
+                                   )
     }
     
     
