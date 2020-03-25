f7a309ca34c3b4e650ac873c816d65ec4616b079
w4356y <w4356y@163.com>
2020-03-25 18:50:00 +0800

    export runApp function

--- R/runApp.R
+++ R/runApp.R
@@ -2,11 +2,11 @@ source("R/ui.R", encoding = "UTF-8")
 source("R/server.R", encoding = "UTF-8")
 
 
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
 
 #' Run the Shiny Application
 #'
 #' @export
-# runApp <- function(){
-#   shinyApp(ui = ui, server = server)
-# }
+ runApp <- function(){
+   shinyApp(ui = ui, server = server)
+ }
