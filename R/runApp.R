90f600e6fae89b7c1c224550f63543f9c9a17622
w4356y <w4356y@163.com>
2020-03-26 14:47:01 +0800

    change source path

--- R/runApp.R
+++ R/runApp.R
@@ -1,5 +1,5 @@
-source("ui.R", encoding = "UTF-8")
-source("server.R", encoding = "UTF-8")
+source("R/ui.R", encoding = "UTF-8")
+source("R/server.R", encoding = "UTF-8")
 shinyApp(ui = ui, server = server)
 
 # runApp <- function(){
