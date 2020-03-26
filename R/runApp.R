1450e8c5cd1d90bfd1f352a1cf3abcb805394d33
w4356y <w4356y@163.com>
2020-03-26 15:03:44 +0800

    rewrite runApp.R function

--- R/runApp.R
+++ R/runApp.R
@@ -1,7 +1,8 @@
 source("R/ui.R", encoding = "UTF-8")
 source("R/server.R", encoding = "UTF-8")
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
 
-# runApp <- function(){
-#   shinyApp(ui = ui, server = server)
-# }
+#'@export
+runApp <- function(){
+   shinyApp(ui = ui, server = server)
+ }
